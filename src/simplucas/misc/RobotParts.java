7e6cc2b0474e9dc246110665a1bee86654d66bee
Lucas Jed <yo6luk@yahoo.com>
2013-02-04 19:40:21 -0600

    Brians awesome new code is ONLINE

--- src/simplucas/misc/RobotParts.java
+++ src/simplucas/misc/RobotParts.java
@@ -6,6 +6,7 @@ package simplucas.misc;
 
 import edu.wpi.first.wpilibj.DriverStationLCD;
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
 //import edu.wpi.first.wpilibj.camera.AxisCamera;
 
 /**
@@ -29,19 +30,31 @@ public class RobotParts {
     //public AxisCamera camera;
     public DriverStationLCD textOutput;
     
-
+    private double time;
     private static RobotParts partInst;
+    public Joystick joy1;
+    public Joystick joy2;
     
-    private RobotParts(){
+    public RobotParts(){
         //Creates the jaguar from the last two sections
         LDrive = new Jaguar(LDriveP);
         RDrive = new Jaguar(RDriveP);
         Hopper = new Jaguar(HopperP);
         Shooter = new Jaguar(ShooterP);
         Arm = new Jaguar(ArmP);
-       textOutput = DriverStationLCD.getInstance();
-       // camera = new AxisCamera("192.168.0.90");
+        textOutput = DriverStationLCD.getInstance();
+        joy1 = new Joystick(1);
+        joy2 = new Joystick(2);
+        // camera = new AxisCamera("192.168.0.90");
     }
+       
+    public void runHopper(double x){
+        time = x;
+        textOutput.println(DriverStationLCD.Line.kUser1, 1, "Speed 1 "+ joy1.getAxis(Joystick.AxisType.kY));
+        textOutput.println(DriverStationLCD.Line.kUser2, 1, "Speed 2 "+ joy2.getAxis(Joystick.AxisType.kY));
+        textOutput.updateLCD();
+    }
+       
     public static RobotParts getInstance(){
         if(partInst == null) //if partInst doesnt exist make it!!
             partInst = new RobotParts();
--- src/simplucas/misc/RobotTemplate.java
+++ src/simplucas/misc/RobotTemplate.java
@@ -4,14 +4,13 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package simplucas.misc;
-
 
 import edu.wpi.first.wpilibj.DriverStationLCD;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the SimpleRobot
@@ -19,85 +18,61 @@ import edu.wpi.first.wpilibj.Timer;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-
 public class RobotTemplate extends SimpleRobot {
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
-    //Doubles are like integers but with decimals like 24.23
-    public double LSpeed;
-    public double RSpeed;
-    //Booleans are either true or false
-    public boolean runHopper;
-    public boolean runArm;
-    public boolean backArm;
-    public boolean runShooter;
-    
-    public void robotInit(){
+    public void robotInit() {
         RobotParts.getInstance();
     }
-    protected void disabled(){
+
+    protected void disabled() {
         stop();
         getWatchdog().setEnabled(false);
     }
-    public void stop(){
-        //Sets all of the motors off before the robot stops
-        LSpeed = 0;
-        RSpeed = 0;
-        runHopper = false;
-        runArm = false;
-        backArm = false;
-        runShooter = false;
-    }
-    public void autonomous() {
 
+    public void stop() {
+        //NOTHING
     }
+
+    public void autonomous() {
+    }
+
+    public void runDrive(RobotParts q) {
+        q.RDrive.set(q.joy1.getAxis(Joystick.AxisType.kY));
+        q.LDrive.set(q.joy2.getAxis(Joystick.AxisType.kY) * -1);
+    }
+
+    public void runShooter(RobotParts q) {
+        q.Shooter.set(q.joy1.getTrigger() ? 0.5 : 0);
+    }
+
+    public void runHopper(RobotParts q) {
+        q.Hopper.set(q.joy1.getRawButton(2) ? 0.5 : 0);
+    }
+
+    public void runArm(RobotParts q) {
+        q.Arm.set(q.joy1.getRawButton(4) ? 0.9 : 0);
+        q.Arm.set(q.joy1.getRawButton(5) ? -0.9 : 0);
+    }
+
     /**
      * This function is called once each time the robot enters operator control.
      */
-    
     public void operatorControl() {
-    while (isOperatorControl() && isEnabled()){ //
-        Joystick joy1 = new Joystick(1);
-        Joystick joy2 = new Joystick(2);
+        while (isOperatorControl() && isEnabled()) {
 
-            //Sets RSpeed as the forward and backward axis of joystick one
-            RSpeed = joy1.getAxis(Joystick.AxisType.kY);
-            LSpeed = joy2.getAxis(Joystick.AxisType.kY);
-            
-            //Sets runHopper to the value of the button
-            runHopper = joy1.getRawButton(2);
-            //Sets runShooter to the value of the trigger also known as Button(1)
-            runShooter = joy1.getTrigger();
-            
-            //Sets runArm to the value of the button
-            runArm = joy1.getRawButton(4);
-            backArm = joy1.getRawButton(5);
-            
-        RobotParts robotParts = RobotParts.getInstance();
-        robotParts.textOutput.println(DriverStationLCD.Line.kUser2, 1, "be printed i hope");
-        robotParts.textOutput.println(DriverStationLCD.Line.kUser1, 1, "Lots of words should");
-        robotParts.textOutput.updateLCD();
-        
-        //Sets the drive values to the speed values from the joystick
-        robotParts.LDrive.set(LSpeed*-1);
-        robotParts.RDrive.set(RSpeed);
-        //Sets the hopper to either on at 0.5 else 0
-        /*
-         *This part calls from robotParts and sets the Hopper jag to the next value
-         *robotParts.Hopper.set(
-         * ? checks runHopper for true if so then it sets Hopper to 0.5
-         * : is a compact else statement
-         * runHopper? .5 : 0);
-         * congrats now you understand a compacted if else statement
-         */      
-  
-        robotParts.Hopper.set(runHopper? .5 : 0);
-        robotParts.Arm.set(runArm? 0.90 : 0);
-        robotParts.Arm.set(backArm? -0.90 : 0);
-        robotParts.Shooter.set(runShooter? .72 : 0);
-        
-        Timer.delay(0.005);
+            RobotParts setOfParts = RobotParts.getInstance();
+
+            setOfParts.runHopper(5);
+
+            runDrive(setOfParts);
+            runHopper(setOfParts);
+            runArm(setOfParts);
+            runShooter(setOfParts);
+
+            Timer.delay(0.05);
         }
     }
 }
